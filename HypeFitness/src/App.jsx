f4f6a0fdc459d8a67b1b78f11a21e72f7ffd792b
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-10-31 16:23:49 +0000

    $git commit

---
+++ HypeFitness/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './assets/HypeFitness-character.png'
+import '../public/HypeFitness-logo.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
