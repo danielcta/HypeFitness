1a8369e1b274c208796e50ee2c2dafb82ae3c1a3
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-11-01 10:12:20 +0000

    $git commit

--- HypeFitness/src/App.jsx
+++ HypeFitness/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
             <label id='lblAboutUs'>Somos uma iniciativa independente dedicada a impulsionar o sucesso profissional de profissionais da área fitness e academias que não possuem aplicativo próprio. Além disso, visamos orientar nossos usuários que estão iniciando sua jornada no mundo fitness, proporcionando um maior conhecimento sobre o assunto e auxiliando-os a atingir seu potencial estético máximo.</label>
           </div>
           <img src="/HypeFitness-character.png" alt="character" id='imgCharacter-default' />
+          jdSDIOFSDAUIFGsafgusaefguiosbadl
         </div>
       </main>
     </>
