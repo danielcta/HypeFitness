ed63288cd7b92cdbf84775db7d4ae6ed95958194
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-11-10 19:48:24 +0000

    $git commit

--- HypeFitness/src/pages/signUp/signUp.jsx
+++ HypeFitness/src/pages/signUp/signUp.jsx
@@ -1,40 +1,51 @@
 import './signUp.css'
-import imgLogin from '../../assets/imgLogin.jpg'
+import imgSignIn from '../../assets/imgSignIn.png'
 
-function SignUp()
-{
+function SignUp() {
 
-    return(
+    return (
         <>
             <div id='signUp'>
-                    <img src={imgLogin} alt="" id='imgLogin' width={'40%'}/>
-                <div id='leftSignUp'  height={'100%'} width={'50%'}>
-                    <img src="/HypeFitnessLogo.png" alt="" width={'100px'} height={'auto'}/>
-                    <div>
-                        <label htmlFor="">Email ou número de telefone</label>
-                        <br />
-                        <input type='text'/>
+                <img src={imgSignIn} alt="" id='imgSignIn' height={'100%'} width={'39.59%'} />
+                <div id='leftSignUp'>
+                    <img src='/HypeFitnessLogo.png' alt="" width={'150px'} className='logo' />
+                    <div className='divData'>
+                        <select name="" id="registerType" className='inputData'>
+                            <option value="UserRegister">Cadastro de Usuário/Profissional</option>
+                            <option value="GymRegister">Cadastro de Academia</option>
+                        </select>
                     </div>
-                    <div>
-                        <label htmlFor="">CPF</label>
-                        <br />
-                        <input type='text'/>
+                    <div id='UserRegister'>
+                        <div className='divData'>
+                            <label htmlFor="">Email</label>
+                            <br />
+                            <input type='text' id='txtEmail' className='inputData' />
+                        </div>
+                        <div className='divData'>
+                            <label htmlFor="">CPF</label>
+                            <br />
+                            <input type='text' id='txtCPF' className='inputData' />
+                        </div>
+                        <div className='divData'>
+                            <label htmlFor="">Data de nascimento</label>
+                            <br />
+                            <input type="date" name="" id="txtBirthDay" className='inputData' />
+                        </div>
+                        <div className='divData'>
+                            <label htmlFor="">Senha</label>
+                            <br />
+                            <input type="password" name="" id="txtPassword" className='inputData' />
+                        </div>
+                        <div className='divData'>
+                            <label htmlFor="">Confirmar senha</label>
+                            <br />
+                            <input type='password' id='txtConfirmPassword' className='inputData' />
+                        </div>
                     </div>
-                    <div>
-                        <label htmlFor="">Data de nascimento</label>
-                        <br />
-                        <input type="date" name="" id="" />
+                    <div id='GymRegister'>
+                    
                     </div>
-                    <div>
-                        <label htmlFor="">Senha</label>
-                        <br />
-                        <input type='text'/>
-                    </div>
-                    <div>
-                        <label htmlFor="">Confirmar senha</label>
-                        <br />
-                        <input type='text'/>
-                    </div>
+                    <input type="submit" value="Cadastrar" className='button' id='btnSignUp-SignUpPage' />      
                 </div>
             </div>
         </>
